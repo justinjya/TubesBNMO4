de405070e2c077cdb3604c3a97342b02b7e65319
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-15 03:17:39 +0700

    Update f06_ubahstok.py

--- f06_ubahstok.py
+++ f06_ubahstok.py
@@ -13,14 +13,14 @@ def ubah_stok(x):
             # Input perubahan stok
             tambah = int(input('Masukkan jumlah: '))
             if tambah < 0 and abs(tambah) > x[i][5]: # Validasi bahwa hasil akhir stok tidak bisa kurang dari 0
-                print('Stok x',x[i][1],'gagal dikurangi karena stok kurang. Stok sekarang: ',x[i][5], '(<',abs(tambah),')')
+                print('Stok game',x[i][1],'gagal dikurangi karena stok kurang. Stok sekarang:',x[i][5], '(<',abs(tambah),')')
             else:
                 # Menambah stok
                 x[i][5] = str(int(x[i][5]) + tambah)
                 if tambah > 0:
-                    print('Stok x', x[i][1],'berhasil ditambahkan. Stok sekarang: ',x[i][5])
+                    print('Stok game', x[i][1],'berhasil ditambahkan. Stok sekarang:',x[i][5])
                 else:
-                    print('Stok x', x[i][1],'berhasil dikurangi. Stok sekarang: ',x[i][5])
+                    print('Stok game', x[i][1],'berhasil dikurangi. Stok sekarang:',x[i][5])
             break
     else: # Jika id game tidak terdaftar dalam data
         print('Tidak ada game dengan ID tersebut!')
