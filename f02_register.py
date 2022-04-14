30abd0b6e5c0fffd7985e6119c246a3f88986063
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-15 01:08:51 +0700

    Update f02_register.py

--- f02_register.py
+++ f02_register.py
@@ -3,31 +3,35 @@ from .functions import*
 def register(x):
     global user
     
+    # Karakter - karakter yang dibolehkan dalam username
     simbol = ['-','_']
     nomor = ['1','2','3','4','5','6','7','8','9','0']
     big = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
     small = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
+    # Input nama, username, dan password
     nama = input('Masukkan nama: ')
     username = input('Masukkan username: ')
     password = input('Masukkan password: ')
 
+    # Validasi apakah username sudah dipakai atau belum
     for i in range(length(x)):
         if username == x[i][1]:
             print('Username', username, 'sudah terpakai, silahkan menggunakan username lain')
             break
     else:
         count = 0
-
+        # Inisialisasi list untuk validasi karakter - karakter dalam username
         temp = [0 for i in range(length(username))]
         for i in range(length(username)):
             temp[i] = username[i]
-
+        # Validasi apakah username sudah sesuai kriteria
         for i in range(length(username)):
             if length(username) < 5:
                 print('Username harus memiliki minimal 5 karakter')
                 break
             else:
+                # Validasi apakah karakter - karakter dalam username sudah sesuai kriteria
                 for i in range(length(username)):
                     if (temp[i] in big) or (temp[i] in small) or (temp[i] in simbol) or (temp[i] in nomor):
                         count += 1
