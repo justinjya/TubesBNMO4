e11093fffd8a1491401c32a43d01ed36416a0064
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-15 03:26:52 +0700

    Update f07_listgametoko.py

--- f07_listgametoko.py
+++ f07_listgametoko.py
@@ -1,13 +1,18 @@
 from .functions import*
 
+# Fungsi list_game_toko menerima parameter y yaitu variabel game
 def list_game_toko(y):
     global game
 
+    # Inisialisasi temporary array kosong untuk meng-copy list data game
     temp = []
+    # Mengisi temporary array dengan list data game
     for i in range(length(y)):
         temp += [[y[i][0],y[i][1],y[i][2],y[i][3],y[i][4],y[i][5]]]
-    # input skema sorting dan melakukan sorting
-    skema = input("Skema sorting : ")
+        
+    # Input skema sorting dan melakukan sorting
+    skema = input('Skema sorting : ')
+    
     # sorted_list = sortedlist(skema)
     valid = False
 
@@ -33,32 +38,33 @@ def list_game_toko(y):
             x = '-'
             valid = True
         else:
-            print("invalid") 
+            print('Skema sorting tidak valid!') 
             skema = input("Skema sorting : ")
 
     if atribut == 'harga':
-        n = 4 # lokasi 'harga' di tabel 'game.csv' ada di kolom ke-4
+        n = 4 # Lokasi 'harga' di tabel 'game.csv' ada di kolom ke-4
     elif atribut == 'tahun':
-        n = 3 # lokasi 'tahun' di tabel 'game.csv' ada di kolom ke-3
+        n = 3 # Lokasi 'tahun' di tabel 'game.csv' ada di kolom ke-3
     elif atribut == 'id':
-        n = 0 # lokasi 'tahun' di tabel 'game.csv' ada di kolom ke-0
-    # selection sort
-
+        n = 0 # Lokasi 'tahun' di tabel 'game.csv' ada di kolom ke-0
+        
+    # Selection sort
     if length(temp) > 1:
         for i in range(length(temp)):
-            ekstrim = i # nilai ekstrim (min/maks)
+            ekstrim = i # Nilai ekstrim (min/maks)
             for j in range(length(temp)):
-                if atribut == 'id':
+                if atribut == 'id': # Sorting berdasarkan 'id' ( Tidak perlu melakukan sorting karena id selalu dalam keadaan sorted ascending )
                     temp                       
                 else:
-                    # selain atribut 'id', diurutinnya manual berdasarkan angka
+                    # Selain atribut 'id', diurutinnya manual berdasarkan angka
                     if x == '-':
                         if (int(temp[ekstrim][n]) < int(temp[j][n])):
                             temp[i], temp[j] = temp[j], temp[i]
                     elif x == '+':
                         if (int(temp[ekstrim][n]) > int(temp[j][n])):
                             temp[i], temp[j] = temp[j], temp[i]
-
+                            
+    # Output list yang sudah diurutkan sesuai atribut
     for i in range(length(temp[0])):
         spaces(temp,i)
     num = [0 for i in range(length(temp))]
