4632bc76ca37423ec91441e7f10081e8c14a091a
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-15 03:31:30 +0700

    Update f09_listgame.py

--- f09_listgame.py
+++ f09_listgame.py
@@ -1,26 +1,29 @@
 from .functions import*
 
+# Fungsi list_game menerima parameter x, y, dan z yaitu variabel game, kepemilikan, user_id
 def list_game(x,y,z):
     global game
     global kepemilikan
     global user_id
 
+    # Inisialisasi jumlah indeks, temporary array untuk menyimpan data 'game_id', dan temporary array untuk menyimpan game sesuai data 'game_id'
     count = 0
     temp = []
     tempo = []
 
+    # Mengisi temporary array dengan data 'game_id'
     for i in range(length(y)):
-        if z == y[i][1]:
-            count += 1
+        if z == y[i][1]: # Validasi user_id yang sesuai dengan data kemepilikan
+            count += 1 # Menambah jumlah indeks sesuai data kepemilikan sesuai 'user_id' dengan 1
             temp += [y[i][0]]
     
+    # Mengisi temporary array dengan game sesuai data 'game_id'
     for i in range(length(x)):
         for j in range(length(temp)):
             if temp[j] == x[i][0]:
                 tempo += [[x[i][0],x[i][1],x[i][2],x[i][3],x[i][4]]]
     
-    print(tempo)
-    
+    # Output daftar game yang dimiliki 'user_id'
     print('Daftar game:')
     if count == 0:
         print('Maaf, kamu belum memiliki game. Ketik perintah "buy_game" untuk beli.')
