8b2478918e649cac56b26496b170dbb1626fbfa9
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-15 01:13:51 +0700

    Update f04_tambahgame.py

--- f04_tambahgame.py
+++ f04_tambahgame.py
@@ -3,12 +3,14 @@ from .functions import*
 def tambah_game(x):
     global game
 
+    # Input nama, kategori, tahun rilis, harga, dan stok game
     nama = input('Masukkan nama game: ')
     kategori = input('Masukkan kategori: ')
     tahun_rilis = input('Masukkan tahun rilis: ')
     harga = input('Masukkan harga: ')
     stok = input('Masukkan stok awal: ')
 
+    # Validasi input nama, kategori, tahun rilis, harga, dan stok game agar sesuai kriteria dan di-daftarkan dalam data
     while (nama != str and nama == '') and (kategori != str and kategori == '') and tahun_rilis != int and harga != int and stok != int:
         print('Mohon masukkan semua informasi mengenai game agar dapat disimpan BNMO.')
         nama = input('Masukkan nama game: ')
