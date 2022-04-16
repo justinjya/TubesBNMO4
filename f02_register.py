4ad0b5834342e890c5b21b033f21dd905eceb961
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-16 17:24:20 +0700

    Update f02_register.py

--- f02_register.py
+++ f02_register.py
@@ -3,12 +3,6 @@ from .functions import*
 # Fungsi register menerima parameter x yaitu variabel user
 def register(x):
     global user
-    
-    # Karakter - karakter yang dibolehkan dalam username
-    simbol = ['-','_']
-    nomor = ['1','2','3','4','5','6','7','8','9','0']
-    big = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
-    small = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
     # Input nama, username, dan password
     nama = input('Masukkan nama: ')
@@ -23,9 +17,7 @@ def register(x):
     else:
         count = 0 # Inisialisasi panjang indeks username
         # Inisialisasi list untuk validasi karakter - karakter dalam username
-        temp = [0 for i in range(length(username))]
-        for i in range(length(username)):
-            temp[i] = username[i]
+        temp = temp = [ord(i) for i in username]
             
         # Looping untuk validasi apakah username sudah sesuai kriteria
         for i in range(length(username)):
@@ -35,7 +27,7 @@ def register(x):
             else:
                 # Validasi apakah karakter - karakter dalam username sudah sesuai kriteria
                 for i in range(length(username)):
-                    if (temp[i] in big) or (temp[i] in small) or (temp[i] in simbol) or (temp[i] in nomor):
+                    if (temp[i] >= 97 and temp[i] <= 122) or (temp[i] >= 65 and temp[i] <= 90) or (temp[i] >= 48 and temp[i] <= 57) or (temp[i] == 45) or (temp[i] == 95):
                         count += 1 # Jika karakter dalam username sesuai dengan kriteria maka tambah jumlah username dengan 1
                 if count == length(username):
                     print('Username', username, 'telah berhasil register ke dalam "Binomo".')
