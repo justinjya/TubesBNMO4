7a4b03545cd71422fec44c310829020932a2d7d6
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-18 19:27:52 +0700

    Update f13_riwayat.py

--- f13_riwayat.py
+++ f13_riwayat.py
@@ -10,7 +10,8 @@ def riwayat(x,y):
     # Mengisi temporary array dengan elemen history
     # x[i][0] yaitu game_id, x[i][1] yaitu nama, x[i][2], yaitu kategori, x[i][3] yaitu tahun rilis, x[i][4] yaitu harga
     for i in range(length(x)):
-        temp += [[x[i][0],x[i][1],x[i][2],x[i][3],x[i][4]]]
+        if y == x[i][3]:
+            temp += [[x[i][0],x[i][1],x[i][2],x[i][4]]]
 
     if length(x) == 0: # Jika tidk memiliki game
         print('Maaf, kamu tidak ada riwayat pembelian game. Ketik perintah "buy_game" untuk membeli.')
@@ -24,6 +25,5 @@ def riwayat(x,y):
         print('Daftar game:')
         # Output daftar game dalam riwayat pembelian sesuai user_id
         for i in range(length(temp)):
-            if y == temp[i][3]:
                 # tempo[i][0] yaitu game_id, tempo[i][1] yaitu nama, tempo[i][2], yaitu kategori, tempo[i][3] yaitu tahun rilis, tempo[i][4] yaitu harga
-                print(str(num[i]) + ' ' + temp[i][0] + ' | ' + temp[i][1] + ' | ' + temp[i][2] + ' | ' + temp[i][4])
+                print(str(num[i]) + ' ' + temp[i][0] + ' | ' + temp[i][1] + ' | ' + temp[i][2] + ' | ' + temp[i][3])
