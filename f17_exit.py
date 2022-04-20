cd8fadb01a3f2b3091899a0539610892bff8af59
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-21 00:11:42 +0700

    Update f17_exit.py

--- f17_exit.py
+++ f17_exit.py
@@ -1,18 +1,13 @@
 from .f16_save import save
 
-# Fungsi exit menerima parameter x, y, z, dan w yaitu variabel user, game, kepemilikan, dan history
-def exit(x,y,z,w):
-    global user
-    global game
-    global kepemilikan
-    global history
-
+# Fungsi exit menerima parameter yaitu variabel user, game, kepemilikan, dan history
+def exit(user,game,kepemilikan,history):
     # Input iya atau tidak
     inpt = input('Apakah Anda mau melakukan penyimpanan file yang sudah diubah? (y/n) ')
 
     # Jika iya maka menjalankan fungsi save
     if inpt == 'Y' or inpt == 'y':
-        save(x,y,z,w)
+        save(user,game,kepemilikan,history)
         return
     elif inpt == 'N' or inpt == 'n':
         return
