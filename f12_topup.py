7e5f0474a4ecdc5e7019737b51a5591d5e122a72
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-20 21:31:47 +0700

    Update f12_topup.py

--- f12_topup.py
+++ f12_topup.py
@@ -1,26 +1,24 @@
 from .functions import*
 
-# Fungsi topup menerima parameter x yaitu variabel user
-def topup(x):
-    global user
-
+# Fungsi topup menerima parameter yaitu variabel user
+def topup(user):
     # Input username dan jumlah penambahan saldo
     username = input('Masukkan username: ')
     tambah = int(input('Masukkan saldo: '))
 
     # Looping untuk mencari indeks username yang terdaftar dalam data
-    for i in range(length(x)):
-        if username == x[i][1]:
+    for i in range(length(user)):
+        if username == user[i][1]:
             # Validasi bahwa saldo akhir tidak bisa kurang dari 0
-            if tambah < 0 and abs(tambah) > x[i][5]:
+            if tambah < 0 and abs(tambah) > user[i][5]:
                 print('Masukan tidak valid')
             else:
                 # Mengubah saldo
-                x[i][5] = str(int(x[i][5]) + tambah)
+                user[i][5] = str(int(user[i][5]) + tambah)
                 if tambah > 0:
-                    print('Top up berhasil. Saldo', x[i][2], 'bertambah menjadi', x[i][5])
+                    print('Top up berhasil. Saldo', user[i][2], 'bertambah menjadi', user[i][5])
                 else:
-                    print('Pengurangan berhasil. Saldo', x[i][2], 'berkurang menjadi', x[i][5])
+                    print('Pengurangan berhasil. Saldo', user[i][2], 'berkurang menjadi', user[i][5])
             break
     # Jika username tidak ditemukan
     else:
