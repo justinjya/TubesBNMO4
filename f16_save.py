b6993a7eecb17713bb6905b24bd3f5da729a3adc
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-20 21:47:08 +0700

    Update f16_save.py

--- f16_save.py
+++ f16_save.py
@@ -1,19 +1,23 @@
 from .functions import*
 
+# Fungsi save menerima parameter yaitu variabel user, game, kepemilikan, dan history
 def save(user,game,kepemilikan,history):
+    # Mengubah array menjadi string untuk di - write ke file csv
     user = arraytocsv(user)
     game = arraytocsv(game)
     kepemilikan = arraytocsv(kepemilikan)
     history = arraytocsv(history)
 
+    # Memasukkan nama folder penyimpanan file csv
     dir = './saves/'
     os.chdir(dir)
     print('Disarankan : ' + str(datetime.today().replace(microsecond=0)))
     folder = input('Masukan nama folder penyimpanan: ')
     
     if not os.path.exists(folder):
-        os.mkdir(folder)
+        os.mkdir(folder) # Membuat folder baru jika nama folder tidak ditemukan
 
+    # Mengisi file csv
     f = open(str(folder)+'/user.csv','w')
     f.write('id;username;nama;password;role;saldo\n')
     f.writelines(user)
