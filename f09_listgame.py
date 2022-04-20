b51c5a083e201f5437bfb60f5231581d2f48b7c7
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-20 21:26:47 +0700

    Update f09_listgame.py

--- f09_listgame.py
+++ f09_listgame.py
@@ -1,28 +1,24 @@
 from .functions import*
 
-# Fungsi list_game menerima parameter x, y, dan z yaitu variabel game, kepemilikan, user_id
-def list_game(x,y,z):
-    global game
-    global kepemilikan
-    global user_id
-
+# Fungsi list_game menerima parameter yaitu variabel game, kepemilikan, user_id
+def list_game(game,kepemilikan,user_id):
     # Inisialisasi jumlah indeks, temporary array untuk menyimpan data 'game_id', dan temporary array untuk menyimpan game sesuai data 'game_id'
     count = 0
     temp = []
     tempo = []
 
     # Mengisi temporary array dengan data 'game_id'
-    for i in range(length(y)):
-        if z == y[i][1]: # Validasi user_id yang sesuai dengan data kemepilikan
+    for i in range(length(kepemilikan)):
+        if user_id == kepemilikan[i][1]: # Validasi user_id yang sesuai dengan data kemepilikan
             count += 1 # Menambah jumlah indeks sesuai data kepemilikan sesuai 'user_id' dengan 1
-            temp += [y[i][0]] # y[i][0] yaitu game_id
+            temp += [kepemilikan[i][0]] # y[i][0] yaitu game_id
     
     # Mengisi temporary array dengan game sesuai data 'game_id'
-    # x[i][0] yaitu game_id, x[i][1] yaitu nama, x[i][2], yaitu kategori, x[i][3] yaitu tahun rilis, x[i][4] yaitu harga
-    for i in range(length(x)):
+    # game[i][0] yaitu game_id, game[i][1] yaitu nama, game[i][2] yaitu kategori, game[i][3] yaitu tahun rilis, game[i][4] yaitu harga
+    for i in range(length(game)):
         for j in range(length(temp)):
-            if temp[j] == x[i][0]:
-                tempo += [[x[i][0],x[i][1],x[i][2],x[i][3],x[i][4]]]
+            if temp[j] == game[i][0]:
+                tempo += [[game[i][0],game[i][1],game[i][2],game[i][3],game[i][4]]]
     
     # Output daftar game yang dimiliki 'user_id'
     print('Daftar game:')
