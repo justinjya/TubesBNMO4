75920104d827e848231f0e6ca97c2228c6b79c5b
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-20 21:21:17 +0700

    Update f06_ubahstok.py

--- f06_ubahstok.py
+++ f06_ubahstok.py
@@ -1,26 +1,24 @@
 from .functions import*
 
 # Fungsi ubah_stok menerima parameter x yaitu varriabel game
-def ubah_stok(x):
-    global game
-
+def ubah_stok(game):
     # Input id game
     id = input('Masukkan ID Game: ')
 
     # Looping untuk mencari indeks game yang terdaftar
-    for i in range(length(x)):
-        if id == x[i][0]:
+    for i in range(length(game)):
+        if id == game[i][0]:
             # Input perubahan stok
             tambah = int(input('Masukkan jumlah: '))
-            if tambah < 0 and abs(tambah) > x[i][5]: # Validasi bahwa hasil akhir stok tidak bisa kurang dari 0
-                print('Stok game',x[i][1],'gagal dikurangi karena stok kurang. Stok sekarang:',x[i][5], '(<',abs(tambah),')')
+            if tambah < 0 and abs(tambah) > game[i][5]: # Validasi bahwa hasil akhir stok tidak bisa kurang dari 0
+                print('Stok game',game[i][1],'gagal dikurangi karena stok kurang. Stok sekarang:',game[i][5], '(<',abs(tambah),')')
             else:
                 # Menambah stok
-                x[i][5] = str(int(x[i][5]) + tambah)
+                game[i][5] = str(int(game[i][5]) + tambah)
                 if tambah > 0:
-                    print('Stok game', x[i][1],'berhasil ditambahkan. Stok sekarang:',x[i][5])
+                    print('Stok game', game[i][1],'berhasil ditambahkan. Stok sekarang:',game[i][5])
                 else:
-                    print('Stok game', x[i][1],'berhasil dikurangi. Stok sekarang:',x[i][5])
+                    print('Stok game', game[i][1],'berhasil dikurangi. Stok sekarang:',game[i][5])
             break
     else: # Jika id game tidak terdaftar dalam data
         print('Tidak ada game dengan ID tersebut!')
