d0ed9a1f704a163ad38358be42aafb44026d97e3
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-21 12:30:51 +0700

    Update b02_kerangajaib.py

--- b02_kerangajaib.py
+++ b02_kerangajaib.py
@@ -1,11 +1,14 @@
 from .functions import*
 
 def kerangajaib():
+    # Untuk menghasilkan seed secara acak dengan LCG
     x = (time.time()*1000)%60
     a = 3
     c = 1
     m = 5
     lcg = round(((a * x) + c) % m)
+    
+    # Input pertanyaan dan kondisi jawaban yang mungkin
     input('Apa pertanyaanmu? ')
     if lcg == 0:
         print('Ya.')
