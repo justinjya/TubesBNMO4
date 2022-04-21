845818e54f30a30f727fa0e22f290c71a3104259
justinjya <103380665+justinjya@users.noreply.github.com>
2022-04-21 12:29:18 +0700

    Update b01_cipher.py

--- b01_cipher.py
+++ b01_cipher.py
@@ -1,5 +1,6 @@
 from .functions import*
 
+# Cipher yang digunakan adalah Caesar Cipher dengan key +6
 def cipher(password):
     temp = [ord(i) for i in password]
     for i in range(length(temp)):
@@ -8,3 +9,12 @@ def cipher(password):
     for i in range(length(temp)):
         ciphered += temp[i]
     return ciphered
+
+def uncipher(password):
+    temp = [ord(i) for i in password]
+    for i in range(length(temp)):
+        temp[i] = chr((temp[i] - 5 - 97) % 26 + 97)
+    unciphered = ''
+    for i in range(length(temp)):
+        unciphered += temp[i]
+    return unciphered
